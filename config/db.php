f58bffccd4961a66ff62eb861dc5197a3f31ce80
cash0151 <cash0151@yahoo.com>
2018-01-06 11:44:45 +0200

    Created project structure

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace config;
+
+class db {
+
+    const HOST = '127.0.0.1';
+    const DB = 'online_store';
+    const USER = 'root';
+    const PASSWORD = '';
+    const CHARSET = 'utf8mb4';
+
+}
